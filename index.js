4e8eb1e6f0879a2eb9b90e237c4c7ccb778692a0
Rodion <valeriaverh@gmail.com>
2025-03-16 22:05:43 +0300

    Lab20

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const path = require("path");
+const data = require("./data");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app
+  .get("/", (req, res) => {
+    const contacts = data.map((d) => `${d.name} - ${d.phone}`);
+
+    res.render("all.handlebars", { contacts });
+  })
+  .get("/Add", (req, res) => {
+    const contacts = data.map((d) => `${d.name} - ${d.phone}`);
+
+    res.render("add.handlebars", { contacts, isContactsBlocked: true });
+  })
+  .get("/Update", (req, res) => {
+    const name = req.query.name;
+    const phone = req.query.phone;
+
+    const contacts = data.map((d) => `${d.name} - ${d.phone}`);
+
+    res.render("update.handlebars", {
+      contacts,
+      isContactsBlocked: true,
+      name,
+      phone,
+    });
+  });
+
+app
+  .post("/Add", (req, res) => {
+    const name = req.body.name;
+    const phone = req.body.phone;
+
+    data.push({ name, phone });
+
+    const contacts = data.map((d) => `${d.name} - ${d.phone}`);
+
+    res.json({ contacts });
+  })
+  .post("/Update", (req, res) => {
+    const name = req.body.name;
+    const phone = req.body.phone;
+
+    const index = data.findIndex((contact) => contact.phone === phone);
+
+    if (index !== -1) data[index].name = name;
+
+    const contacts = data.map((d) => `${d.name} - ${d.phone}`);
+
+    res.json({ contacts });
+  })
+  .post("/Delete", (req, res) => {
+    const phone = req.body.phone;
+
+    const index = data.findIndex((contact) => contact.phone === phone);
+
+    if (index !== -1) data.splice(index, 1);
+
+    const contacts = data.map((d) => `${d.name} - ${d.phone}`);
+
+    res.json({ contacts });
+  });
+
+  const PORT = process.env.PORT || 4000;
+
+  app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+  
